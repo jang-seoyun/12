3d2b43581d5c6aa6971ed4d1b89e407061fd4e67
jang-seoyun <rachel2343@naver.com>
2025-11-27 00:09:16 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+{
+	srand( (unsigned) (time(NULL)) );
+	printf("=============================================\n");
+	printf("*********************************************\n");
+	printf("           SHARK ISLAND GAME START           \n");
+	printf("*********************************************\n");
+	printf("=============================================\n");
+	
+	printf("=============================================\n");
+	printf("*********************************************\n");
+	printf("           SHARK ISLAND GAME END            \n");
+	printf("*********************************************\n");
+	printf("=============================================\n");
+	
+	
+	
+	
+		return 0;
+}
