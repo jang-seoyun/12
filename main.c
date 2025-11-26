2cfc6b923a5593860ba4cc1694d09fa92d0b77c9
jang-seoyun <rachel2343@naver.com>
2025-11-27 01:13:08 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "board.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) 
 {
 	srand( (unsigned) (time(NULL)) );
+	//opening
 	printf("=============================================\n");
 	printf("*********************************************\n");
 	printf("           SHARK ISLAND GAME START           \n");
 	printf("*********************************************\n");
-	printf("=============================================\n");
+	printf("=============================================\n"); 
 	
+	//strp 1. initialization (player name setting, variables)
+	
+	//step 2. turn play (do-while)
+	// 2-1. status printing
+	// 2-2. roll die
+	// 2-3. move(result)
+	// 2-4. change turn, shark move
+	
+	//step 3. game end (winner printing)
+	
+	//ending
 	printf("=============================================\n");
 	printf("*********************************************\n");
 	printf("           SHARK ISLAND GAME END            \n");
