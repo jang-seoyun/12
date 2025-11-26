25d9b51389020bc305471c56e004ad28c5aa4c8e
jang-seoyun <rachel2343@naver.com>
2025-11-27 02:56:42 +0900

    Update board.c

--- board.c
+++ board.c
@@ -7,8 +7,10 @@ int board_getBoardCoin(int pos);
 #endif
 
 #define N_BOARD                 20
-#define BOARDSTATUS_NOK          1
+#define BOARDSTATUS_NOK         1
 #define BOARD_NOK               0
+#define N_COINPOS               12
+#define MAX_COIN                4
 
 static int board_status[N_BOARD];
 static int board_coin[N_BOARD];
@@ -29,4 +31,39 @@ void board_printBoardStatus(void)
 	}
 	printf("|\n"); 
 	printf("------------------ BOARD STATUS ---------------------");
+}
+
+void board_getBoardStatus(int pos)
+{
+	return board_status[pos];
+}
+
+int board_getBoardCoin(int pos)
+{
+	return board_coin[pos];
+}
+
+int board_initBoard(void)
+{
+	int i;
+	//initialize arrays
+	for (i=0;i<N_BOARD;i++);
+	{
+		board_status[i]=BOARDSTATUS_NOK;
+		board_coin[i]=0;
+	}
+	//allocate coins
+	for (i=0;i<N_COINPOS;i++)
+	{
+		int flag_allocated=0;
+		do{
+			int coinpos = rand()%N_BOARD;
+			if (board_coin[coinpos]==0)
+			{
+				board_coin[coinpos] = rand()%MAX_COIN +1;
+				flag_allocated=1;
+			}
+			
+		} while(flag_allocated==0);
+	}
 }
